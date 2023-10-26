844e0d47b01df681fe8f5a62b7c529e019d71978
Joohyung Ryu <kalpafilms@hotmail.com>
2023-10-25 21:00:43 -0400

    Add Boost library

--- NumberToWord/main.cpp
+++ NumberToWord/main.cpp
@@ -6,16 +6,17 @@
 //
 
 #include <iostream>
+#include <boost/algorithm/string.hpp>
 
 int main(int argc, const char *argv[])
 {
-    std::string number;
-
-    std::cout << "Enter a number to convert, or 'q' to exit: ";
-    std::cin >> number;
-    std::cout << std::endl;
-
+    std::string number{};
     std::cout << number << std::endl;
+    while (boost::algorithm::to_upper_copy(number) != "Q") {
+        std::cout << "Enter a number to convert, or 'q' to exit: ";
+        std::cin >> number;
+        std::cout << number << std::endl;
+    }
 
     return 0;
 }
