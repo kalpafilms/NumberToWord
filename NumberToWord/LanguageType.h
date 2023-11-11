cbe15d4b027db3a5620b5f24e46c34b0ce18c780
Joohyung Ryu <Joohyung.Ryu@outlook.com>
2023-11-11 00:03:51 -0500

    Add language type enum class

---
+++ NumberToWord/LanguageType.h
@@ -0,0 +1,17 @@
+//
+//  LanguageType.h
+//  NumberToWord
+//
+//  Created by Joohyung Ryu on 2023-11-11.
+//
+
+#ifndef LanguageType_h
+#define LanguageType_h
+
+enum class Language : char
+{
+    English,
+    Korean
+};
+
+#endif /* LanguageType_h */
